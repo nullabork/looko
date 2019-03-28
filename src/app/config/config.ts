a9497e69f3c471115c0e95489b74a239b68fdfa5
aaron <aaron@aaron-m.co.nz>
2019-03-28 22:38:08 +1300

    endpoints

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -6,11 +6,11 @@ export interface IConfig {
 
 let envs = {
   "development" :  {
-    "api" : "https://api.webdiggo.nullabork.dev:39541/api/v1"
+    "api" : "https://115.70.110.212:39541/api/v1"
   },
 
   "production" : {
-    "api" : "https://api.webdiggo.nullabork.dev:39541/api/v1"
+    "api" : "https://115.70.110.212:39541/api/v1"
   }
 }
 
