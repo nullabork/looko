b8db21aa1615c03e58679a9deb55d9b9ea77b63f
aaron <aaron@aaron-m.co.nz>
2019-03-30 17:07:15 +1300

    a;

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,15 +2,27 @@
 
 export interface IConfig {
   api : string;
+  pager : {
+    count : number;
+    offset : number;
+  }
 }
 
 let envs = {
   "development" :  {
-    "api" : "https://115.70.110.212:39541/api/v1"
+    "api" : "https://115.70.110.212:39541/api/v1",
+    "pager" : {
+      "count" : 20,
+      "offset" : 0
+    }
   },
 
   "production" : {
-    "api" : "https://115.70.110.212:39541/api/v1"
+    "api" : "https://115.70.110.212:39541/api/v1",
+    "pager" : {
+      "count" : 20,
+      "offset" : 0
+    }
   }
 }
 
